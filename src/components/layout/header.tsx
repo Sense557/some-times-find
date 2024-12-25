9d2fa1bb139c60f81e8a405328a5a32d1038154d
Shakti <mehershakti1020@gmail.com>
2024-12-25 08:12:04 +0530

    profile part done

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,10 +1,24 @@
+import { Layout, Space } from "antd";
 import CurrentUser from "./current-user";
 
 const Header = () => {
+  const headerStyles = {
+    display: "flex",
+    justifyContent: "flex-end",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    padding: "0 24px",
+    position: "sticky",
+    top: 0,
+    zIndex: 999,
+  };
+
   return (
-    <div>
-      <CurrentUser />
-    </div>
+    <Layout.Header style={headerStyles}>
+      <Space align="center" size="middle">
+        <CurrentUser />
+      </Space>
+    </Layout.Header>
   );
 };
 
