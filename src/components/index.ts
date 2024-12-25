f8bc4b42e66eb9dddbfae5de9981765b2c62a5f5
Shakti <mehershakti1020@gmail.com>
2024-12-25 19:11:53 +0530

    issue is haapening as we cannot getting data

---
+++ src/components/index.ts
@@ -0,0 +1,15 @@
+import DealsChart from "./home/deals-chart";
+import UpcomingEvents from "./home/upcoming-events";
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+import KanbanColumnSkeleton from "./skeleton/kanban";
+import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+import ProjectCardSkeleton from "./skeleton/project-card";
+
+export {
+  DealsChart,
+  UpcomingEvents,
+  UpcomingEventsSkeleton,
+  KanbanColumnSkeleton,
+  LatestActivitiesSkeleton,
+  ProjectCardSkeleton,
+};
